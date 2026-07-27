27a374fc736094cbceb9ac6fda6abeb51315b917
Jaymax-cyber <jossycode0@gmail.com>
2026-07-27 10:10:16 +0100

    updt

--- components/dashboard/MobileSidebar.tsx
+++ components/dashboard/MobileSidebar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import {
     Sheet,
     SheetContent,
@@ -9,45 +11,104 @@ import Logo from "../shared/Logo";
 import NavItem from "./NavItem";
 
 import { sidebarLinks } from "@/constants/sidebar";
+import { hasRole } from "@/lib/permissions";
+
 
 interface Props {
     open: boolean;
     setOpen: (value: boolean) => void;
 }
 
+
 export default function MobileSidebar({
     open,
     setOpen,
 }: Props) {
+
+
+    const [user, setUser] = useState<any>(null);
+
+
+
+    useEffect(() => {
+
+        const storedUser =
+            localStorage.getItem("user");
+
+
+        if (storedUser) {
+
+            setUser(
+                JSON.parse(storedUser)
+            );
+
+        }
+
+    }, []);
+
+
+
+
+    const filteredLinks =
+        sidebarLinks.filter((item) =>
+            user
+                ? hasRole(
+                    user,
+                    item.roles
+                )
+                : false
+        );
+
+
+
     return (
+
         <Sheet
             open={open}
             onOpenChange={setOpen}
         >
+
             <SheetContent
                 side="left"
                 className="w-72 p-0"
             >
+
                 <div className="border-b p-6">
+
                     <Logo />
+
                 </div>
+
+
 
                 <nav className="space-y-2 p-5">
 
-                    {sidebarLinks.map((item) => (
+
+                    {filteredLinks.map((item) => (
 
                         <div
                             key={item.href}
-                            onClick={() => setOpen(false)}
+                            onClick={() =>
+                                setOpen(false)
+                            }
                         >
-                            <NavItem {...item} />
+
+                            <NavItem
+                                {...item}
+                            />
+
                         </div>
 
                     ))}
 
+
                 </nav>
 
+
             </SheetContent>
+
         </Sheet>
+
     );
+
 }
